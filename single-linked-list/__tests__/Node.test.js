a454706c57ef9e903759c920b04fb0fb29564303
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-08-12 23:03:18 +0300

    feat: should links given node to the previos one

--- single-linked-list/__tests__/Node.test.js
+++ single-linked-list/__tests__/Node.test.js
@@ -7,4 +7,13 @@ describe("Single Linked List - Node", () => {
     expect(node.value).toBe(1);
     expect(node.next).toBe(null);
   });
+
+  test("should links given node to the previos one", () => {
+    const node2 = new Node(2);
+    const node1 = new Node(1, node2);
+
+    expect(node1.value).toBe(1);
+    expect(node1.next).toBe(node2);
+    expect(node1.next.value).toBe(node2.value);
+  });
 });
